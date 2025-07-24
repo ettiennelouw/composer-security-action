ba8d785fcfc2de0adf04c8469d68af5ca575a66d
Ettienne Louw <ettienne.louwki@gmail.com>
2025-07-24 11:28:15 +0200

    Additional git security notice fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "The attribute is: $1"
 
+sh -c "git config --global --add safe.directory $PWD"
+
 if [[ -z "$1" || "$1" == "audit" ]]; then
     # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
     composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json

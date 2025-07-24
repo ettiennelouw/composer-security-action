426d39d1b87fc2c3a6512d623cc0a7ac3ae3c073
Ettienne Louw <ettienne.louwki@gmail.com>
2025-07-24 10:46:55 +0200

    Updates to entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "The attribute is: $1"
 
-# if [ -z "$1" || "$1" == "audit" ]; then
+if [[ -z "$1" || "$1" == "audit" ]]; then
     # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
     composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
     status=$?
@@ -10,18 +10,16 @@ echo "The attribute is: $1"
     cat composer-audit-output.json
 
     php /jsonToOutput.php composer-audit-output.json >> $GITHUB_STEP_SUMMARY
-# fi
+fi
 
-# if [ "$1" == "test" ]; then
+if [[ "$1" == "test" ]]; then
+    # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
+    composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
+    status=$?
 
-#     echo $1
-#     # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-#     # composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-#     status=$?
+    cat composer-audit-output.json
 
-#     # cat composer-audit-output.json
-
-#     # php /jsonToOutput.php composer-audit-output.json >> $GITHUB_STEP_SUMMARY
-# fi
+    php /jsonToOutput.php composer-audit-output.json >> $GITHUB_STEP_SUMMARY
+fi
 
 exit $status;

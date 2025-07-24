b64b3694edcc09c7181a7b680baa90064bf798e8
Ettienne Louw <ettienne.louwki@gmail.com>
2025-07-24 12:20:11 +0200

    Remove all issues in entry point

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM composer:latest
 
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
 COPY jsonToOutput.php /jsonToOutput.php
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 sh -c "git config --global --add safe.directory $PWD"
 
 if [[ -z "$1" || "$1" == "audit" ]]; then
-    # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-    composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-    status=$?
+    composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
+    # composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
+    # status=$?
 
     cat composer-audit-output.json
 
@@ -13,13 +13,12 @@ if [[ -z "$1" || "$1" == "audit" ]]; then
 fi
 
 if [[ "$1" == "test" ]]; then
-    # composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-    composer audit --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
-    status=$?
+    composer $1 --format=json --no-scripts --no-plugins --no-interaction > composer-audit-output.json
+    # status=$?
 
     cat composer-audit-output.json
 
     php /jsonToOutput.php composer-audit-output.json >> $GITHUB_STEP_SUMMARY
 fi
 
-exit $status;
+# exit $status;

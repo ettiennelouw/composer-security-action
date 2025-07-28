0cefd28106fa7dff66c73dca0d6b40159bd51305
Ettienne Louw <ettienne.louwki@gmail.com>
2025-07-28 11:00:13 +0200

    Added validate

--- jsonValidateToOutput.php
+++ jsonValidateToOutput.php
@@ -4,23 +4,19 @@ renderFlavoredMarkdown($argv[1]);
 
 function renderFlavoredMarkdown(string $filename): void
 {
-    $json = file_get_contents($filename);
-    $data = json_decode($json, true);
+    $data = file_get_contents($filename);
 
-    echo renderAdvisories($data['advisories']);
-    echo "\n\n";
-    echo renderAbandonedPackages($data['abandoned'] ?? []);
-    echo "\n";
+    echo renderValiditiers($data);
 }
 
 /**
- * @param array<string, array<array{advisoryId: string, packageName: string, affectedVersions: string, title: string, cve: string, link: string, reportedAt: string}>> $advisories
+ * @param $data<string>
  * @return string
  */
-function renderAdvisories(array $advisories): string
+function renderValiditiers($data): string
 {
     $output = <<<MARKDOWN
-## :exclamation: :exclamation: Security Vulnerabilities
+## :warning: Validate composer.json & composer.lock
 
 MARKDOWN;
 
@@ -30,61 +26,8 @@ MARKDOWN;
     }
 
     $output .= <<<MARKDOWN
-| Package | Severity | CVE | Affected versions | Reported at |
-| ------- | -------- | --- | ----------------- | ----------- |
+$data
 MARKDOWN;
-
-    foreach ($advisories as $issues) {
-        foreach ($issues as $issue) {
-            $cve = sprintf(
-                '[%s](%s): %s',
-                $issue['cve'],
-                $issue['link'],
-                $issue['title'],
-            );
-
-            $output .= sprintf(
-                "\n| %s | %s | %s | %s |",
-                $issue['packageName'],
-                $issue['severity'],
-                $cve,
-                $issue['affectedVersions'],
-                DateTime::createFromFormat(DATE_ATOM, $issue['reportedAt'])->format('Y-m-d H:i:s'),
-            );
-        }
-    }
-
-    return $output;
-}
-
-/**
- * @param array<string, string> $abandonedPackages
- * @return string
- */
-function renderAbandonedPackages(array $abandonedPackages): string
-{
-    $output = <<<MARKDOWN
-## :warning: Abandoned Packages
-
-MARKDOWN;
-
-    if (empty($abandonedPackages)) {
-        $output .= 'No abandoned packages';
-        return $output;
-    }
-
-    $output .= <<<MARKDOWN
-| Abandoned Package | Suggested Replacement |
-| ----------------- | --------------------- |
-MARKDOWN;
-
-    foreach ($abandonedPackages as $abandoned => $replacement) {
-        $output .= sprintf(
-            "\n| %s | %s |",
-            $abandoned,
-            $replacement
-        );
-    }
 
     return $output;
 }
